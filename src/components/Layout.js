fa727938b5a73db758aa451d2a50c9e4c661a88a
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-10-24 12:15:29 -0300

    Pizarra styles basic

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Nav } from './Nav';
+import styled from 'styled-components';
 
+const LayoutStyles = styled.div`
+  max-width: 1600px;
+  text-align: center;
+`;
 export const Layout = ({ children }) => {
   return (
     <>
       <Nav />
-      {children}
+      <LayoutStyles>{children}</LayoutStyles>
     </>
   );
 };
--- src/pages/BasquetBapp/JugadaScreen.js
+++ src/pages/BasquetBapp/JugadaScreen.js
@@ -2,9 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Layout } from '../../components/Layout';
 import { getPlayById } from '../../store/actions/playActions';
-import { Player } from 'video-react';
+import styled from 'styled-components';
 
 import { storage } from '../../firebase/firebase';
+
+const JugadaStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const AbbreviationToWord = {
+  B: 'Base',
+  E: 'Escolta',
+  AL: 'Alero',
+  P: 'Pivot',
+  AP: 'Ala-Pivot',
+};
+
 export const JugadaScreen = ({ match }) => {
   const dispatch = useDispatch();
   const { play, loading, error } = useSelector((state) => state.play);
@@ -26,8 +40,32 @@ export const JugadaScreen = ({ match }) => {
         <p>Cargando</p>
       ) : (
         <>
-          <p>{play.nombreDeLaJugada}</p>
-          <Player playsInline src={showVideo} />
+          <h1>{play.nombreDeLaJugada}</h1>
+          {/* <Player playsInline src={showVideo}>
+            <ControlBar>
+              <ReplayControl seconds={10} order={1.1} />
+              <ForwardControl seconds={30} order={1.2} />
+              <CurrentTimeDisplay order={4.1} />
+              <TimeDivider order={4.2} />
+              <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
+              <VolumeMenuButton disabled />
+            </ControlBar>
+          </Player> */}
+          <JugadaStyles>
+            <div>
+              <h2>Posicion Asistente</h2>
+              {AbbreviationToWord[play.posicionAsistente]}
+              <h2>Posicion Tirador</h2>
+              {AbbreviationToWord[play.posicionTirador]}
+            </div>
+
+            <div>
+              <video width='600' height='450' controls>
+                <source src={showVideo} type='video/mp4' />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </JugadaStyles>
         </>
       )}
     </Layout>
