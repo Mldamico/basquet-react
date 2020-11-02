25a76a2f56cb61e6fd296fd230df119c335ebeba
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-11-02 13:36:04 -0300

    Jugada component

---
+++ src/components/Jugada.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link, useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import swal from 'sweetalert';
+import { removePlay } from '../store/actions/playActions';
+
+const JugadaStyles = styled.div`
+  background-color: var(--red);
+  border-radius: 10px;
+  padding: 3rem;
+  color: #fff;
+  h2 {
+    background-color: var(--yellow);
+    transition: all 0.2s ease-in-out;
+    text-align: center;
+    color: #fff;
+    max-width: 60%;
+    margin: 0 auto;
+    margin-bottom: 2rem;
+
+    &:hover {
+      transform: rotate(8deg);
+    }
+  }
+  a {
+    text-decoration: none;
+  }
+  img {
+    width: 300px;
+    height: 300px;
+  }
+`;
+
+const ButtonsStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  button {
+    margin: 0.5rem 0;
+    background-color: var(--yellow);
+  }
+
+  button:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const Jugada = ({ play }) => {
+  let history = useHistory();
+  const dispatch = useDispatch();
+  const remove = (id) => {
+    swal({
+      title: 'Seguro que queres eliminar la jugada?',
+      text: 'Una vez que hayas eliminado la jugada no podras verla nuevamente',
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        dispatch(removePlay(id));
+        swal('Se elimino la jugada correctamente!', {
+          icon: 'success',
+        });
+      } else {
+        swal('No se elimino la jugada!');
+      }
+    });
+  };
+  return (
+    <JugadaStyles>
+      <Link to={`/jugadas/${play.id}`}>
+        <h2>{play.nombreDeLaJugada}</h2>
+      </Link>
+      {!play.image ? (
+        <img src={`assets/jugada2.jpeg`} alt={play.nombreDeLaJugada} />
+      ) : (
+        <img src={play.image} alt={play.nombreDeLaJugada} />
+      )}
+
+      <p>Asistente: {play.posicionAsistente}</p>
+      <p>Tirador: {play.posicionTirador}</p>
+      <p>Puntos: {play.valorDelPuntoPorDefecto}</p>
+      <ButtonsStyles>
+        <button onClick={() => history.push(`/jugadas/${play.id}`)}>Ver</button>
+        <button onClick={() => history.push(`/pizarra/edit/${play.id}`)}>
+          Editar
+        </button>
+        <button onClick={() => remove(play.id)}>Eliminar</button>
+      </ButtonsStyles>
+    </JugadaStyles>
+  );
+};
--- src/pages/BasquetBapp/JugadasScreen.js
+++ src/pages/BasquetBapp/JugadasScreen.js
@@ -2,12 +2,12 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { Layout } from '../../components/Layout';
-import { getPlays, removePlay } from '../../store/actions/playActions';
+import { getPlays } from '../../store/actions/playActions';
 import { Link } from 'react-router-dom';
 import { Search } from '../../components/Search';
-import swal from 'sweetalert';
 import { Message } from '../../components/Message';
 import { CenterLoading } from '../../components/CenterLoading';
+import { Jugada } from '../../components/Jugada';
 
 const Button = styled.button`
   margin-bottom: 5rem;
@@ -19,73 +19,21 @@ const Button = styled.button`
   }
 `;
 
-const ButtonsStyles = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  button {
-    margin: 0.5rem 0;
-  }
-
-  button:hover {
-    transform: scale(1.1);
-  }
-`;
-
 const JugadasStyles = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   justify-items: center;
-  max-width: 70%;
   margin: 0 auto;
   gap: 10rem;
-
-  h2 {
-    background-color: var(--yellow);
-    transition: all 0.2s ease-in-out;
-    text-align: center;
-    color: #fff;
-    max-width: 60%;
-    margin: 0 auto;
-
-    &:hover {
-      transform: rotate(8deg);
-    }
-  }
-  a {
-    text-decoration: none;
-  }
-  img {
-    width: 300px;
-    height: 300px;
-  }
 `;
 
-export const JugadasScreen = ({ history }) => {
+export const JugadasScreen = () => {
   const dispatch = useDispatch();
   const { error, loading, plays } = useSelector((state) => state.play);
   useEffect(() => {
     dispatch(getPlays());
   }, [dispatch]);
 
-  const remove = (id) => {
-    swal({
-      title: 'Seguro que queres eliminar la jugada?',
-      text: 'Una vez que hayas eliminado la jugada no podras verla nuevamente',
-      icon: 'warning',
-      buttons: true,
-      dangerMode: true,
-    }).then((willDelete) => {
-      if (willDelete) {
-        dispatch(removePlay(id));
-        swal('Se elimino la jugada correctamente!', {
-          icon: 'success',
-        });
-      } else {
-        swal('No se elimino la jugada!');
-      }
-    });
-  };
   return (
     <Layout>
       {loading ? (
@@ -101,33 +49,7 @@ export const JugadasScreen = ({ history }) => {
 
           <JugadasStyles>
             {plays.map((play) => (
-              <div key={play.id}>
-                {!play.image ? (
-                  <img
-                    src={`assets/jugada2.jpeg`}
-                    alt={play.nombreDeLaJugada}
-                  />
-                ) : (
-                  <img src={play.image} alt={play.nombreDeLaJugada} />
-                )}
-                <Link to={`/jugadas/${play.id}`}>
-                  <h2>{play.nombreDeLaJugada}</h2>
-                </Link>
-                <p>Asistente: {play.posicionAsistente}</p>
-                <p>Tirador: {play.posicionTirador}</p>
-                <p>Puntos: {play.valorDelPuntoPorDefecto}</p>
-                <ButtonsStyles>
-                  <button onClick={() => history.push(`/jugadas/${play.id}`)}>
-                    Ver
-                  </button>
-                  <button
-                    onClick={() => history.push(`/pizarra/edit/${play.id}`)}
-                  >
-                    Editar
-                  </button>
-                  <button onClick={() => remove(play.id)}>Eliminar</button>
-                </ButtonsStyles>
-              </div>
+              <Jugada play={play} key={play.id} />
             ))}
           </JugadasStyles>
         </>
