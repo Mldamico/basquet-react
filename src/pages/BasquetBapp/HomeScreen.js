87b204e966393c8e7df94408f55dbeb60386ab5e
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-11-02 11:32:19 -0300

    Profile on home Screen and animation

--- src/pages/BasquetBapp/HomeScreen.js
+++ src/pages/BasquetBapp/HomeScreen.js
@@ -3,8 +3,10 @@ import { Layout } from '../../components/Layout';
 import styled from 'styled-components';
 import { Title } from '../../components/Title';
 import { Link } from 'react-router-dom';
-
-const LoginStyles = styled.div`
+import { useSelector } from 'react-redux';
+import { CenterLoading } from '../../components/CenterLoading';
+import Unknown from '../../assets/unknown.jpg';
+const HomeStyles = styled.div`
   height: 100vh;
   background-image: url('/ball.webp');
   background-size: 25%;
@@ -12,18 +14,40 @@ const LoginStyles = styled.div`
   background-position: 50% 60%;
   position: relative;
 
+  .user-data {
+    position: absolute;
+    background-color: var(--red);
+    top: 30%;
+    left: 5%;
+    width: 25%;
+    border-radius: 10px;
+    border: 1px solid #fff;
+    img {
+      width: 80%;
+      margin-top: 2rem;
+    }
+    h4,
+    p {
+      color: #fff;
+    }
+  }
+
   a {
     color: var(--red);
     text-decoration: none;
     font-size: 2.5rem;
   }
 
-  p {
+  .first p,
+  .second p,
+  .third p,
+  .fourth p {
     padding-left: 1rem;
+    color: #fff;
     display: inline-block;
     opacity: 0;
-    transform: translateX(-50%);
-    transition: all 0.5s ease-in;
+    animation-name: out;
+    animation-duration: 1s;
   }
 
   .first,
@@ -32,9 +56,31 @@ const LoginStyles = styled.div`
   .fourth {
     &:hover {
       p {
-        transform: translateX(0);
-        opacity: 1;
+        animation-name: in;
+        animation-duration: 1s;
       }
+    }
+  }
+
+  @keyframes in {
+    0% {
+      opacity: 0;
+      transform: translateY(-100%);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes out {
+    0% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+    100% {
+      transform: translateY(100%);
+      opacity: 0;
     }
   }
 
@@ -67,35 +113,50 @@ const LoginStyles = styled.div`
 `;
 
 export const HomeScreen = () => {
+  const { user, loading, error } = useSelector((state) => state.auth);
   return (
     <Layout>
-      <LoginStyles>
-        <Title size={7}>BASQUETBAPP</Title>
-        <div className='first'>
-          <Link to='/jugadas'>
-            <i class='fas fa-chalkboard'></i>
-            <p>Jugadas</p>
-          </Link>
-        </div>
-        <div className='second'>
-          <Link to='/estadisticas'>
-            <i class='fas fa-chart-bar'></i>
-            <p>Estadisticas</p>
-          </Link>
-        </div>
-        <div className='third'>
-          <Link to='/datos'>
-            <i class='far fa-clipboard'></i>
-            <p>Tomar Datos</p>
-          </Link>
-        </div>
-        <div className='fourth'>
-          <Link to='/gestionjugadores'>
-            <i class='fas fa-people-arrows'></i>
-            <p>Gestion Jugadores</p>
-          </Link>
-        </div>
-      </LoginStyles>
+      {loading ? (
+        <CenterLoading />
+      ) : (
+        <HomeStyles>
+          <Title size={7}>BASQUETBAPP</Title>
+          <div className='user-data'>
+            <div>
+              <img
+                src={user.urlFoto ? user.urlFoto : Unknown}
+                alt={user.nombre}
+              />
+            </div>
+            <h4>{user.usuario}</h4>
+            <p>{user.tipo}</p>
+          </div>
+          <div className='first'>
+            <Link to='/jugadas'>
+              <i className='fas fa-chalkboard'></i>
+              <p>Jugadas</p>
+            </Link>
+          </div>
+          <div className='second'>
+            <Link to='/estadisticas'>
+              <i className='fas fa-chart-bar'></i>
+              <p>Estadisticas</p>
+            </Link>
+          </div>
+          <div className='third'>
+            <Link to='/datos'>
+              <i className='far fa-clipboard'></i>
+              <p>Tomar Datos</p>
+            </Link>
+          </div>
+          <div className='fourth'>
+            <Link to='/gestionjugadores'>
+              <i className='fas fa-people-arrows'></i>
+              <p>Gestion Jugadores</p>
+            </Link>
+          </div>
+        </HomeStyles>
+      )}
     </Layout>
   );
 };
