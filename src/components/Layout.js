89979df84ace8fb0c329f942d9b4c0f43b8ae013
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-11-02 12:57:56 -0300

    Home styles in another file

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Nav } from './Nav';
 import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import { logout } from '../store/actions/authActions';
 
 const LayoutStyles = styled.div`
   /* width: 1600px; */
@@ -8,12 +10,32 @@ const LayoutStyles = styled.div`
   /* height: 100vh; */
   text-align: center;
   margin: 0 auto;
+
+  .logout-button {
+    position: fixed;
+    top: 2rem;
+    right: 2rem;
+    background-color: var(--red);
+    color: #fff;
+    padding: 0.5rem;
+    font-weight: bold;
+    font-size: 1.6rem;
+  }
 `;
 export const Layout = ({ children }) => {
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
   return (
     <>
       {/* <Nav /> */}
-      <LayoutStyles>{children}</LayoutStyles>
+      <LayoutStyles>
+        <button className='logout-button' onClick={logoutHandler}>
+          LOGOUT
+        </button>
+        {children}
+      </LayoutStyles>
     </>
   );
 };
---
+++ src/styles/HomeStyles.js
@@ -0,0 +1,128 @@
+import styled from 'styled-components';
+
+export const HomeStyles = styled.div`
+  height: 100vh;
+  position: relative;
+
+  .centered {
+    position: fixed;
+    top: 55%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    img {
+      transition: 1s all ease-in-out;
+      width: 50%;
+    }
+
+    img:hover {
+      transform: rotate(360deg);
+    }
+  }
+  .user-data {
+    position: absolute;
+    background-color: var(--red);
+    top: 30%;
+    left: 5%;
+    width: 25%;
+    border-radius: 10px;
+    border: 1px solid #fff;
+    img {
+      width: 80%;
+      margin-top: 2rem;
+    }
+    h4,
+    p {
+      color: #fff;
+    }
+  }
+
+  a {
+    color: var(--red);
+    text-decoration: none;
+    font-size: 2.5rem;
+  }
+
+  .first p,
+  .second p,
+  .third p,
+  .fourth p {
+    padding-left: 1rem;
+    color: #fff;
+    display: inline-block;
+    opacity: 0;
+    animation-name: out;
+    animation-duration: 1s;
+  }
+
+  .first,
+  .second,
+  .third,
+  .fourth {
+    &:hover {
+      p {
+        animation-name: in;
+        animation-duration: 1s;
+      }
+    }
+  }
+
+  @keyframes in {
+    0% {
+      opacity: 0;
+      transform: translateY(-100%);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes out {
+    0% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+    100% {
+      transform: translateY(100%);
+      opacity: 0;
+    }
+  }
+
+  i {
+    font-size: 5rem;
+  }
+
+  .first {
+    position: fixed;
+    top: 32%;
+    left: 63%;
+  }
+  .second {
+    position: fixed;
+    top: 46%;
+    left: 68%;
+  }
+
+  .third {
+    position: fixed;
+    top: 59%;
+    left: 68%;
+  }
+
+  .fourth {
+    position: fixed;
+    top: 71%;
+    left: 63%;
+  }
+  .logout-button {
+    position: fixed;
+    top: 2rem;
+    right: 2rem;
+    background-color: var(--red);
+    color: #fff;
+    padding: 0.5rem;
+    font-weight: bold;
+    font-size: 1.6rem;
+  }
+`;
