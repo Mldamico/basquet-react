6ed6f16225873dcbda360eefc29353cd2641687d
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-12-01 11:58:54 -0300

    Changes on partido styles

--- src/pages/BasquetBapp/PartidoScreen.js
+++ src/pages/BasquetBapp/PartidoScreen.js
@@ -1,206 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Layout } from '../../components/Layout';
-import styled from 'styled-components';
+import {
+  ChangesStyles,
+  ChronoStyles,
+  ChronometterStyles,
+  PlayStyles,
+} from '../../styles/PartidoStyles';
 import Countdown, { zeroPad } from 'react-countdown';
 import { useDispatch, useSelector } from 'react-redux';
 import Unknown from '../../assets/unknown.jpg';
 import { getPlays } from '../../store/actions/playActions';
 import { registerPlay } from '../../store/actions/matchActions';
-const ChronoStyles = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  background-color: var(--red);
-  margin: 10rem auto 0;
-  width: 80%;
-  border-radius: 10px;
-  border: 1px solid #fff;
-  color: #fff;
-  position: relative;
-  .button_container {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    margin: 1rem;
-  }
-
-  button {
-    outline: none;
-    height: 5rem;
-    width: 5rem;
-    border-radius: 100%;
-    background-color: var(--yellow);
-    padding: 2rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: 1px solid #fff;
-  }
-
-  button:active {
-    background-color: var(--red);
-  }
-  button:disabled {
-    background-color: var(--black);
-  }
-`;
-
-const ChangesStyles = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  align-items: center;
-  background-color: var(--red);
-  margin: 10rem auto;
-  width: 80%;
-  border-radius: 10px;
-  border: 1px solid #fff;
-  color: #fff;
-
-  .selected {
-    background-color: var(--yellow);
-  }
-
-  .players_list {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    border: 1px solid #fff;
-    border-radius: 10px;
-    overflow: hidden;
-    margin-bottom: 1rem;
-    p {
-      margin: 0;
-      margin-left: 1rem;
-    }
-  }
-
-  .player {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 3rem;
-  }
-
-  button:disabled {
-    color: #000;
-  }
-`;
-
-const PlayStyles = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  background-color: var(--red);
-  margin: 10rem auto;
-  width: 80%;
-  border-radius: 10px;
-  border: 1px solid #fff;
-  color: #fff;
-
-  select {
-    width: 25rem;
-    border-radius: 10px;
-    overflow: hidden;
-    font-size: 2rem;
-    margin: 0 auto;
-    display: block;
-    text-align-last: center;
-  }
-  option {
-    text-align: center;
-  }
-  input {
-    width: 10rem;
-    border-radius: 10px;
-    text-align-last: center;
-    border: 0.5rem;
-  }
-  .switch {
-    position: relative;
-    display: inline-block;
-    width: 60px;
-    height: 34px;
-  }
-
-  .switch input {
-    opacity: 0;
-    width: 0;
-    height: 0;
-  }
-
-  .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-  }
-
-  .slider:before {
-    position: absolute;
-    content: '';
-    height: 26px;
-    width: 26px;
-    left: 4px;
-    bottom: 4px;
-    background-color: white;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-  }
-
-  input:checked + .slider {
-    background-color: var(--yellow);
-  }
-
-  input:focus + .slider {
-    box-shadow: 0 0 1px var(--yellow);
-  }
-
-  input:checked + .slider:before {
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px);
-    transform: translateX(26px);
-  }
-
-  .slider.round {
-    border-radius: 34px;
-  }
-
-  .slider.round:before {
-    border-radius: 50%;
-  }
-  button {
-    outline: none;
-    background-color: var(--yellow);
-    border: 1px solid #fff;
-    border-radius: 10px;
-    margin: 1rem;
-    &:disabled {
-      background-color: #000;
-    }
-  }
-`;
-
-const ChronometterStyles = styled.div`
-  display: flex;
-  margin-top: 2rem;
-  align-items: center;
-  justify-content: center;
-  font-size: 4rem;
-  width: 20rem;
-  height: 20rem;
-  border-radius: 50%;
-  background-color: var(--yellow);
-  border: 1px solid #fff;
-`;
 
 const renderer = ({ minutes, seconds, completed }) => {
   return (
@@ -268,14 +78,10 @@ export const PartidoScreen = () => {
     tiradorAlternativa,
   ]);
   const startHandler = () => {
-    // console.log(buttonEl.current);
-
     buttonEl.current.start();
-    // setStartedTimer(true);
   };
   const stopHandler = () => {
     buttonEl.current.pause();
-    // setStartedTimer(false);
   };
 
   const increaseQuarter = () => {
@@ -287,7 +93,6 @@ export const PartidoScreen = () => {
     const jugadorIndex = titulares.findIndex(
       (titular) => titular.id === jugadorSaliente.id
     );
-    console.log(jugadorIndex);
     setTitulares((titulare) => {
       titulare.splice(jugadorIndex, 1, jugadorEntrante);
       console.log(titulare);
@@ -308,9 +113,6 @@ export const PartidoScreen = () => {
     );
     const tiempo = `${buttonEl.current.state.timeDelta.minutes}:${buttonEl.current.state.timeDelta.seconds}`;
     if (!alternativa) {
-      console.log(jugadaSeleccionada);
-
-      console.log(jugadaActual);
       const positionTirador = Object.keys(positions).find((key) => {
         return positions[key] === jugadaActual.posicionTirador;
       });
@@ -332,11 +134,8 @@ export const PartidoScreen = () => {
         idJugada: jugadaSeleccionada,
         tiempo,
       };
-      console.log(play);
       dispatch(registerPlay(play));
     } else {
-      console.log(valorAlternativa);
-
       const asistente = titulares.find(
         (titular) => titular.id === Number(asistenteAlternativa)
       );
@@ -357,7 +156,6 @@ export const PartidoScreen = () => {
         idJugada: jugadaSeleccionada,
         tiempo,
       };
-      console.log(play);
       dispatch(registerPlay(play));
     }
   };
@@ -383,7 +181,12 @@ export const PartidoScreen = () => {
             <button onClick={stopHandler} disabled={quarter > 4 + extraTime}>
               <i className='fas fa-pause'></i>
             </button>
-            <button onClick={() => setExtraTime((e) => e + 1)}>+</button>
+            <button
+              disabled={quarter < 4}
+              onClick={() => setExtraTime((e) => e + 1)}
+            >
+              +
+            </button>
           </div>
         </div>
       </ChronoStyles>
